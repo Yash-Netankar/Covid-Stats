03ab3cc877095fb200c63a8c0bbb33b6ff7b701d
Yash-Netankar <netankaryash2002@gmail.com>
2021-01-31 11:43:18 +0530

    News Site Embedded

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + "/public"));
 
 // engine
 app.set("view engine", "hbs");
-app.set("views", __dirname+"/views");
+app.set("views", __dirname + "/views");
 
 // partials
 hbs.registerPartials(__dirname + "/views/partials");
@@ -20,52 +20,60 @@ hbs.registerPartials(__dirname + "/views/partials");
  Routing
 ***********/
 // all data
-app.get("/", (req, res)=>{
-    covid.countries({sort:'cases'}).then(data =>{
-        res.render("home", {data:data, single:false});
+app.get("/", (req, res) => {
+    covid.countries({ sort: 'cases' }).then(data => {
+        res.render("home", { data: data, single: false });
     })
-    .catch(err=>{
-        console.log(err);
-    });
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 
 // yesterday data
-app.get("/yesterday", (req,res)=>{
-    covid.yesterday.countries({sort:'cases'}).then(data =>{
-        res.render("home", {data:data, single:false});
+app.get("/yesterday", (req, res) => {
+    covid.yesterday.countries({ sort: 'cases' }).then(data => {
+        res.render("home", { data: data, single: false });
     })
-    .catch(err=>{
-        console.log(err);
-    });
+        .catch(err => {
+            console.log(err);
+        });
 });
 
 // specific countries
-app.get("/search", (req,res)=>{
+app.get("/search", (req, res) => {
 
     let arr = req.query.value.split(",");
-    if(arr.length===1){
-        covid.countries({country:arr[0]}).then(data =>{
-            res.render("home", {data:data, single:true});
-        }).catch(err=>{
+    if (arr.length === 1) {
+        covid.countries({ country: arr[0] }).then(data => {
+            res.render("home", { data: data, single: true });
+        }).catch(err => {
             console.log(err);
         });
     }
-    else{
-        covid.countries({country:req.query.value}).then(data =>{
-            res.render("home", {data:data, single:false});
-        }).catch(err=>{
+    else {
+        covid.countries({ country: req.query.value }).then(data => {
+            res.render("home", { data: data, single: false });
+        }).catch(err => {
             console.log(err);
         });
     }
 });
 
+// News page
+app.get("/news", (req, res) => {
+    res.render("news");
+});
+
+
 // about page
-app.get("/about",(req,res)=>{
+app.get("/about", (req, res) => {
     res.render("about");
 });
 
+
+
 // running
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Running on Port ${port}`);
 });
